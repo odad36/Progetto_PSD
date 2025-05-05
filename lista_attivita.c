8b499aea8124f14a02724d698076b18d2f0c8df8
Dario Spartano <dado091605@gmail.com>
2025-05-05 16:07:48 +0200

    scritte due funzioni in lista_attivita.c

--- lista_attivita.c
+++ lista_attivita.c
@@ -7,3 +7,22 @@ struct nodo {
     attivita dato;
     struct nodo* next;
 };
+
+lista_attivita crea_lista(void) {
+    return NULL; //una lista vuota è esattamente un puntatore NULL, questa funzione serve solamente per rispettare l'information hiding
+}
+void stampa_lista(lista_attivita lst);
+
+lista_attivita inserisci_attivita(lista_attivita lst, attivita att) { // non c'è bisogno di "pos", l'inserimento può essere semplicemente effettuato in testa
+    struct nodo* nuovo = malloc(sizeof(struct nodo));
+    if (nuovo == NULL) {
+        printf("Errore: memoria insufficiente\n");
+        return lst;
+    }
+    nuovo->dato = att;
+    nuovo->next = lst;
+    return nuovo;
+}
+void rimuovi_attivita(lista_attivita, char*);
+attivita cerca_attivita(lista_attivita, char*);
+int conta_attivita(lista_attivita);
--- lista_attivita.h
+++ lista_attivita.h
@@ -5,7 +5,7 @@
 typedef struct nodo* lista_attivita;  //typedef "opaco" per rispettare l'information hiding
 lista_attivita crea_lista(void);
 void stampa_lista(lista_attivita);
-void inserisci_attivita(lista_attivita, attivita);
+lista_attivita inserisci_attivita(lista_attivita, attivita);
 void rimuovi_attivita(lista_attivita, char*);
 attivita cerca_attivita(lista_attivita, char*);
 int conta_attivita(lista_attivita);
